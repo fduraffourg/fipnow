601d8e24d571d757ff9e3a578b56a791497e6206
Florian Duraffourg <f.duraffourg@gmail.com>
2018-06-13 12:58:48 +0200

    Change default.nix

--- default.nix
+++ default.nix
@@ -1,32 +1,14 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "default", doBenchmark ? false }:
-
-let
-
-  inherit (nixpkgs) pkgs;
-
-  f = { mkDerivation, aeson, base, lens, lens-aeson, stdenv, text
-      , time, wreq
-      }:
-      mkDerivation {
-        pname = "fipnow";
-        version = "0.1.0.0";
-        src = ./.;
-        isLibrary = false;
-        isExecutable = true;
-        executableHaskellDepends = [
-          aeson base lens lens-aeson text time wreq
-        ];
-        license = stdenv.lib.licenses.bsd3;
-      };
-
-  haskellPackages = if compiler == "default"
-                       then pkgs.haskellPackages
-                       else pkgs.haskell.packages.${compiler};
-
-  variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
-
-  drv = variant (haskellPackages.callPackage f {});
-
-in
-
-  if pkgs.lib.inNixShell then drv.env else drv
+{ mkDerivation, aeson, base, lens, lens-aeson, stdenv, text, time
+, wreq
+}:
+mkDerivation {
+  pname = "fipnow";
+  version = "0.1.0.0";
+  src = ./.;
+  isLibrary = false;
+  isExecutable = true;
+  executableHaskellDepends = [
+    aeson base lens lens-aeson text time wreq
+  ];
+  license = stdenv.lib.licenses.bsd3;
+}
